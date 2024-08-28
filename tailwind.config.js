2e3a006f60caaff8bbb98dae91ab30b2affd60e8
tevinowino <tevinowino65@gmail.com>
2024-08-28 15:23:45 +0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'brand-forest-blue': '#133040',
+        'brand-luminous-green':'#dadb2d',
+      },
+    },
+  },
+  plugins: [],
+}
+
